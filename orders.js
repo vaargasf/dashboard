d5809b41b21070a0c8007431009112675e076ffc
Felipe Vargas <142313509+vaargasf@users.noreply.github.com>
2023-10-23 20:17:57 -0300

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript',
+        productNumber: '85743',
+        paymentStatus: 'Adequado',
+        status: 'Pendente'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '97245',
+        paymentStatus: 'Reembolsado',
+        status: 'Recusado'
+    },
+    {
+        productName: 'FullStack Course',
+        productNumber: '36452',
+        paymentStatus: 'Pago',
+        status: 'Ativo'
+    },
+]
